fc64d80b0361d879dffbde4886aa190fe473a2f2
yashphalle <82230267+yashphalle@users.noreply.github.com>
2022-05-29 15:45:09 +0530

    .

--- server.js
+++ server.js
@@ -32,11 +32,30 @@ app.get('/test',(req,res)=>{
 })
 
 app.post('/check',(req,res)=>{
+    const a=[];
+    a.push(req.body.q1,req.body.q2,req.body.q3,req.body.q4,req.body.q5,req.body.q6,req.body.q7,req.body.q8,req.body.q9,req.body.q10);
+    console.log(a);
+var result=0;
     for (let index = 0; index < 10; index++) {
-        var q=res.body."q.+${index}";
-        const element = array[index];
-        
+        const element = a[index];
+        if(element==1){
+            result=result+100;
+
+        }
+        else if(element==2){
+            result=result+75;
+
+        }
+        else if(element==3){
+            result=result+50;
+
+        }
+        else if(element==4){
+            result=result+25;
+
+        }
     }
+    res.render('resultpage',{result})
 })
 
 const port=3000;app.listen(port, hostname, () => {
